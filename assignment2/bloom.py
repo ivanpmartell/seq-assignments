7e1aede338d70bbbf6240aad7d8f4542151d3f5e
Ivan Perez <ivan.pmartell@hotmail.com>
2020-11-11 01:45:08 -0800

    Finished problem 7

--- assignment2/bloom.py
+++ assignment2/bloom.py
@@ -1,4 +1,5 @@
 import sys
+import math
 
 def MurmurHash64A(k,l,seed):
     m = 0xc6a4a7935bd1e995
@@ -40,18 +41,42 @@ def MurmurHash64A(k,l,seed):
     h ^= h >> r
     return h
 
+def best_size(n: int, p: float):
+    return int(-(n*math.log(p))/math.pow(math.log(2.0),2.0))
+
+def best_hashing(n: int, m: int):
+    return int((m/n)*math.log(2.0))
+
 input_file = sys.argv[1]
 threshold = sys.argv[2]
-hashes = dict[int, int]()
+length = 8
+n = 1999982 #estimated number of kmers
+m = best_size(n, 0.1)
+h = max(1,best_hashing(n, m))
+bloom_filter = [0 for i in range(m)]
+print("Estimated size:", m)
+print("Estimated amount of hashing functions:", h)
+
 for r in FASTA(input_file, fai=False):
     s = r.seq
     kmers = s.kmers[Kmer[20]](1)
+    kmers_set = set[Kmer[20]]()
     for k in kmers:
-        try:
-            hashes[MurmurHash64A(k, 8, 73473)] += 1
-        except:
-            hashes[MurmurHash64A(k, 8, 73473)] = 1
-
-for k, v in hashes.items():
-    if v > int(threshold):
-        print(v)
+        kmers_set.add(k)
+        for i in range(h):
+            index = MurmurHash64A(k, length, i) % len(bloom_filter)    
+            bloom_filter[index] += 1
+    for k in kmers_set:
+        below_threshold = False
+        counts = list[int]()
+        for i in range(h):
+            index = MurmurHash64A(k, length, i) % len(bloom_filter)
+            current_count = bloom_filter[index]
+            if current_count < int(threshold):
+                below_threshold = True
+                break
+            else:
+                counts.append(current_count)
+        if below_threshold == False:
+            print(max(counts), k)
+        
